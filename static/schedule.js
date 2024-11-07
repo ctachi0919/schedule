7032f2d0eabcc702749e3230e9488774849f7c1d
c.tachi <c.tachi@saishunkansys.com>
2024-11-07 23:01:20 +0700

    change frontend

---
+++ static/schedule.js
@@ -0,0 +1,4 @@
+function changeText() {
+  // Get the element with id="message" and change its text content
+  document.getElementById("message").innerText = "You clicked the button! The text is changed.";
+}

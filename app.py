3706126ef4720e5b07ba0b24d84a7391bcd73097
c.tachi <c.tachi@saishunkansys.com>
2025-02-07 15:59:41 +0700

    sidebar

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-# Home route
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-# API route
+# Post test
 @app.route('/api/data', methods=['POST'])
 def api_data():
     data = request.get_json()
     response = {'message': f'Hello, {data["name"]}!'}
     return jsonify(response)
+
+# Main
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()

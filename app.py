65c3777d01456440fb3d8bee9b3b7dce883a9574
ctachi0919 <clover3707@gmai.com>
2025-02-09 16:02:34 +0700

    test mysql

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import pymysql
 
 app = Flask(__name__)
 
@@ -12,6 +13,20 @@ def api_data():
 # Main
 @app.route('/')
 def home():
+    conn = pymysql.connect(
+        host='Tachinya529.mysql.pythonanywhere-services.com',
+        user='Tachinya529',
+        password='chiki529',
+        database='Tachinya529$schedule'
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SHOW TABLES;")
+    print(cursor.fetchall())
+
+    cursor.close()
+    conn.close()
+
     return render_template('index.html')
 
 
